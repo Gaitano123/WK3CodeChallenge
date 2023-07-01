c602a5eac21a631054f34c6d55b3c92b164684c7
Gaitano <gaitano.george@student.moringaschool.com>
2023-07-02 01:56:02 +0300

    Styling

--- index.js
+++ index.js
@@ -15,16 +15,18 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     function movieDetails(film){
         const pack = document.createElement('div')
+        pack.className = 'pack-film'
         const availableTicket = film.capacity - film.tickets_sold
         pack.innerHTML= `
-        <p>${film.title}</p>
+        <p class="title">${film.title}</p>
         <img src="${film.poster}"/>
-        <p>${film.description}</p>
+        <p class="description">Description: ${film.description}</p>
         <p>Runtime: ${film.runtime}</p>
         <p>Showtime: ${film.showtime}</p>
         <p> Capacity: ${film.capacity}</p>
         <p>Tickets Sold: ${film.tickets_sold}</p>
         <p>Available Tickets: ${availableTicket}</p>
+        <button>Buy Ticket</button>
         `
         document.getElementById('film-content').appendChild(pack)
     }
